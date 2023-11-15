3e347eae14c75a917db2ba6360c67b7f0d81a117
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2023-11-15 20:47:17 +0530

    feat: languages and countries drop down

---
+++ script.js
@@ -0,0 +1,133 @@
+var showCountry = document.querySelector('.show-country')
+var countriesDropDown = document.querySelector('.countries-drop-down');
+var showLanguages = document.querySelector('.show-language');
+var languageDropDown = document.querySelector('.languages-drop-down')
+var countiresAdded = false;
+
+
+showCountry.addEventListener('click', () => {
+    if (countriesDropDown.style.display === 'block') {
+        countriesDropDown.style.display = 'none';
+    } else {
+        if (languageDropDown.style.display === 'block') {
+            languageDropDown.style.display = 'none';
+        }
+        if (!countiresAdded) {
+            countiresWithFlagName();
+            countiresAdded = true;
+        }
+
+        countriesDropDown.style.display = 'block';
+    }
+})
+
+var languagesAdded = false;
+showLanguages.addEventListener('click', () => {
+    if (languageDropDown.style.display === 'block') {
+        languageDropDown.style.display = 'none';
+    } else {
+        if (countriesDropDown.style.display == 'block') {
+            countriesDropDown.style.display = 'none';
+        }
+        if (!languagesAdded) {
+            language();
+            languagesAdded = true;
+        }
+
+        languageDropDown.style.display = 'block';
+    }
+})
+
+//drop down for countires
+var mainCountryName = document.querySelector('.country-name');
+var countryFlag = document.querySelector('.country-flag')
+function countiresWithFlagName() {
+
+
+    var countries = [
+        { src: './assets/icons/flags/Australia.png', countryName: 'Australia' },
+        { src: './assets/icons/flags/Bahrain.png', countryName: 'Bahrain' },
+        { src: './assets/icons/flags/Chile.png', countryName: 'Chile' },
+        { src: './assets/icons/flags/Egypt.png', countryName: 'Egypt' },
+        { src: './assets/icons/flags/Greece.png', countryName: 'Greece' },
+        { src: './assets/icons/flags/India.png', countryName: 'India' },
+        { src: './assets/icons/flags/Ireland.png', countryName: 'Ireland' },
+        { src: './assets/icons/flags/Italy.png', countryName: 'Italy' },
+        { src: './assets/icons/flags/japan.png', countryName: 'Japan' },
+        { src: './assets/icons/flags/Jordan.png', countryName: 'Jordan' },
+        { src: './assets/icons/flags/Kuwait.png', countryName: 'Kuwait' },
+        { src: './assets/icons/flags/Malaysia.png', countryName: 'Malaysia' },
+        { src: './assets/icons/flags/New Zealand.png', countryName: 'New Zealand' },
+        { src: './assets/icons/flags/Peru.png', countryName: 'Peru' },
+        { src: './assets/icons/flags/Romania.png', countryName: 'Romania' },
+        { src: './assets/icons/flags/Saudi Arabia.png', countryName: 'Saudi Arabia' },
+        { src: './assets/icons/flags/Switzerland.png', countryName: 'Switzerland' },
+        { src: './assets/icons/flags/uk.png', countryName: 'UK' },
+        { src: './assets/icons/flags/Vietnam.png', countryName: 'Vietnam' },
+        { src: './assets/icons/flags/Yemen.png', countryName: 'Yemen' },
+    ]
+
+
+    function displayData(item, idx) {
+
+        var countriesDropDownContainer = document.querySelector('.countries-drop-down-container');
+
+        var flagWithName = document.createElement('div');
+        flagWithName.classList.add('flag-with-name')
+
+        var flagImage = document.createElement('img');
+        var countryName = document.createElement('p');
+
+        flagImage.src = item.src;
+        countryName.textContent = item.countryName;
+
+        flagWithName.appendChild(flagImage);
+        flagWithName.appendChild(countryName);
+
+        countriesDropDownContainer.appendChild(flagWithName);
+
+        flagWithName.addEventListener('click', function () {
+            console.log('flagWithName', flagWithName.textContent);
+            countryFlag.setAttribute('src', flagImage.src)
+            mainCountryName.textContent = flagWithName.textContent;
+
+            countriesDropDown.style.display = 'none';
+        })
+    }
+
+
+    countries.forEach((item, idx) => {
+        displayData(item, idx)
+    })
+}
+
+//drop down for languages
+var showLanguageOnLanguageBox = document.querySelector('.show-language-on-language-box');
+function language() {
+    var languages = ['English', 'Türkçe', 'हिंदी', 'Português(BR)', 'Indonesian', 'Português(PT)', 'Español', 'Čeština', 'Slovenčina', 'Polish', 'Italian', 'Vietnamese']
+
+    function displayData(item, idx) {
+
+        var languagesDropDownContainer = document.querySelector('.languages-drop-down-container');
+
+        var languagesWithName = document.createElement('div');
+        languagesWithName.classList.add('language-with-name')
+
+        var languagesName = document.createElement('p');
+        languagesName.textContent = item;
+
+        languagesWithName.appendChild(languagesName);
+
+        languagesDropDownContainer.appendChild(languagesWithName);
+
+        languagesName.addEventListener('click', function () {
+            showLanguageOnLanguageBox.textContent = languagesName.textContent;
+            languageDropDown.style.display = 'none';
+        })
+    }
+
+
+    languages.forEach((item, idx) => {
+        displayData(item, idx)
+    })
+}
