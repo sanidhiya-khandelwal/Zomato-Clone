a16c7f90a0e78963945acdcc59853ea902ec39ba
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2023-12-10 11:44:16 +0530

    feat: setting up the page for restaurant detail

---
+++ restrauntDetail.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    selectedRestauraunt = JSON.parse(localStorage.getItem('selectedRestauraunt'));
+    console.log(selectedRestauraunt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // clearing the item from localStorage once it's retrieved
+    // localStorage.removeItem('selectedRestauraunt');
+});
